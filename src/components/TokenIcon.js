8975496a363db2dd595cd724196b2227f8e7b118
cryptofish7 <cryptofish7@protonmail.com>
2021-07-12 01:01:47 -0400

    Add token icons

--- src/components/TokenIcon.js
+++ src/components/TokenIcon.js
@@ -15,7 +15,7 @@ export default function TokenIcon({ id, ...rest }) {
   const classes = useStyles();
   const src = useMemo(
     () =>
-      `https://raw.githubusercontent.com/sushiswap/assets/master/blockchains/ethereum/assets/${toChecksumAddress(
+      `https://raw.githubusercontent.com/traderjoe-xyz/joe-tokenlists/main/logos/${toChecksumAddress(
         id
       )}/logo.png`,
     [id]
