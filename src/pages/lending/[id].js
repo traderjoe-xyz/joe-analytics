3349b78bed32aaf200a3512439d29e04232b9d19
cryptofish7 <yocryptofish7@gmail.com>
2022-06-05 11:14:37 +0800

    Trigger deploy

--- src/pages/lending/[id].js
+++ src/pages/lending/[id].js
@@ -4,10 +4,17 @@ import {
   BarChart,
   BasicTable,
   KPI,
-  TokenIcon
+  TokenIcon,
 } from "app/components";
 import { withStyles } from "@material-ui/core/styles";
-import { Box, Card, CardContent, Grid, Paper, Typography } from "@material-ui/core";
+import {
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 import {
   currencyFormatter,
   decimalFormatter,
@@ -15,74 +22,64 @@ import {
   marketDayDatasQuery,
   liquidationDayDatasQuery,
   useInterval,
-  marketsQuery
+  marketsQuery,
 } from "app/core";
-import {
-  getMarket
-} from "app/core/api";
+import { getMarket } from "app/core/api";
 import ProgressBar from "../../components/ProgressBar";
 import Head from "next/head";
 import { ParentSize } from "@visx/responsive";
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-function LendingPage() {
+function LendingPage(props) {
   const router = useRouter();
 
   if (router.isFallback) {
     return <AppShell />;
   }
 
-  const id = router.query.id
-  const totalLiquidityUSD = router.query.tl
+  const id = router.query.id;
+  const totalLiquidityUSD = router.query.tl;
 
-  const SupplyText = withStyles({
-  })(Typography);
+  const SupplyText = withStyles({})(Typography);
 
-  const BorrowText = withStyles({
-  })(Typography);
+  const BorrowText = withStyles({})(Typography);
 
   const {
     data: { market },
-  } = useQuery(marketQuery,
-    {
-      variables: {
-        id
-      }
-    }
-  );
+  } = useQuery(marketQuery, {
+    variables: {
+      id,
+    },
+  });
 
   const {
     data: { marketDayDatas },
-  } = useQuery(marketDayDatasQuery,
-    {
-      variables: {
-        markets: [id]
-      }
-    }
-  );
+  } = useQuery(marketDayDatasQuery, {
+    variables: {
+      markets: [id],
+    },
+  });
 
   const {
     data: { liquidationDayDatas },
-  } = useQuery(liquidationDayDatasQuery ,
-    {
-      variables: {
-        seizedAddress: [id]
-      }
-    }
-  );
+  } = useQuery(liquidationDayDatasQuery, {
+    variables: {
+      seizedAddress: [id],
+    },
+  });
 
-  const sortedMarketDayDatas = marketDayDatas.map(data => {
-    return {
-      date: data.date,
-      totalSupplyUSD: data.totalSupplyUSD,
-      totalBorrowsUSD: data.totalBorrowsUSD,
-      totalReservesUSD: data.totalReservesUSD
-    }
-  }).sort((a, b) =>
-    Number(a.date) > Number(b.date) ? 1 : -1
-  )
+  const sortedMarketDayDatas = marketDayDatas
+    .map((data) => {
+      return {
+        date: data.date,
+        totalSupplyUSD: data.totalSupplyUSD,
+        totalBorrowsUSD: data.totalBorrowsUSD,
+        totalReservesUSD: data.totalReservesUSD,
+      };
+    })
+    .sort((a, b) => (Number(a.date) > Number(b.date) ? 1 : -1));
 
   let cumulativeBorrowsUSD = [];
   let cumulativeSupplyUSD = [];
@@ -103,19 +100,34 @@ function LendingPage() {
     });
   });
 
-  const marketChartDatas = { cumulativeBorrowsUSD, cumulativeSupplyUSD, cumulativeReservesUSD }
+  const marketChartDatas = {
+    cumulativeBorrowsUSD,
+    cumulativeSupplyUSD,
+    cumulativeReservesUSD,
+  };
 
-  let mergedLiquidationDayDatas = []
+  let mergedLiquidationDayDatas = [];
 
-  liquidationDayDatas.forEach(data => {
-    let existing = mergedLiquidationDayDatas.filter(function(pastData, i) {
+  liquidationDayDatas.forEach((data) => {
+    let existing = mergedLiquidationDayDatas.filter(function (pastData, i) {
       return pastData.date == data.date;
     });
     if (existing.length) {
       let existingIndex = mergedLiquidationDayDatas.indexOf(existing[0]);
-      mergedLiquidationDayDatas[existingIndex].underlyingCollateralSeizedAmountUSD = Number(mergedLiquidationDayDatas[existingIndex].underlyingCollateralSeizedAmountUSD) + Number(data.underlyingCollateralSeizedAmountUSD)
+      mergedLiquidationDayDatas[
+        existingIndex
+      ].underlyingCollateralSeizedAmountUSD =
+        Number(
+          mergedLiquidationDayDatas[existingIndex]
+            .underlyingCollateralSeizedAmountUSD
+        ) + Number(data.underlyingCollateralSeizedAmountUSD);
     } else {
-      data = {date: data.date, underlyingCollateralSeizedAmountUSD: Number(data.underlyingCollateralSeizedAmountUSD)};
+      data = {
+        date: data.date,
+        underlyingCollateralSeizedAmountUSD: Number(
+          data.underlyingCollateralSeizedAmountUSD
+        ),
+      };
       mergedLiquidationDayDatas.push(data);
     }
   });
@@ -135,19 +147,25 @@ function LendingPage() {
     await Promise.all([getMarket]);
   }, 1800000);
 
-  const supplyAPY =
-    decimalFormatter.format(parseFloat(market.supplyRate * 100).toFixed(2))
-  const borrowAPY =
-    decimalFormatter.format(parseFloat(market.borrowRate * 100).toFixed(2))
-  const utilizationRate = (Number(market.totalBorrows)/(Number(market.cash) + Number(market.totalBorrows) - Number(market.reserves))) * 100
-  const liquidityUSD = (market.cash - market.reserves) * market.underlyingPriceUSD
+  const supplyAPY = decimalFormatter.format(
+    parseFloat(market.supplyRate * 100).toFixed(2)
+  );
+  const borrowAPY = decimalFormatter.format(
+    parseFloat(market.borrowRate * 100).toFixed(2)
+  );
+  const utilizationRate =
+    (Number(market.totalBorrows) /
+      (Number(market.cash) +
+        Number(market.totalBorrows) -
+        Number(market.reserves))) *
+    100;
+  const liquidityUSD =
+    (market.cash - market.reserves) * market.underlyingPriceUSD;
 
   return (
     <AppShell>
       <Head>
-        <title>
-          Trader Joe Analytics
-        </title>
+        <title>Trader Joe Analytics</title>
       </Head>
       <Box display="flex" alignItems="center" marginBottom="30px">
         <TokenIcon id={market.underlyingAddress} />
@@ -156,19 +174,23 @@ function LendingPage() {
         </Typography>
       </Box>
 
-      <Grid container spacing={6} style={{ height: "40%"}}>
+      <Grid container spacing={6} style={{ height: "40%" }}>
         <Grid item xs={12} md={6}>
           <Card variant="outlined">
-            <CardContent style={{display: "flex", displayDirection: "col"}}>
+            <CardContent style={{ display: "flex", displayDirection: "col" }}>
               <Box>
                 <Typography variant="subtitle2" component="div">
                   Total Supply
                 </Typography>
                 <SupplyText variant="h4">
-                  {currencyFormatter.format(market.totalSupply * market.exchangeRate * market.underlyingPriceUSD)}
+                  {currencyFormatter.format(
+                    market.totalSupply *
+                      market.exchangeRate *
+                      market.underlyingPriceUSD
+                  )}
                 </SupplyText>
               </Box>
-              <Box ml={6} style={{marginLeft: "auto"}}>
+              <Box ml={6} style={{ marginLeft: "auto" }}>
                 <Typography variant="subtitle2" component="div">
                   Supply APY
                 </Typography>
@@ -182,33 +204,35 @@ function LendingPage() {
             variant="outlined"
             style={{ height: 300, marginTop: "40px", position: "relative" }}
           >
-          <ParentSize>
-            {({ width, height }) => (
-              <AreaChart
-                title="Total Supply (USD)"
-                data={marketChartDatas.cumulativeSupplyUSD}
-                width={width}
-                height={height}
-                margin={{ top: 125, right: 0, bottom: 0, left: 0 }}
-                tooltipDisabled
-                overlayEnabled
-              />
-            )}
-          </ParentSize>
-        </Paper>
+            <ParentSize>
+              {({ width, height }) => (
+                <AreaChart
+                  title="Total Supply (USD)"
+                  data={marketChartDatas.cumulativeSupplyUSD}
+                  width={width}
+                  height={height}
+                  margin={{ top: 125, right: 0, bottom: 0, left: 0 }}
+                  tooltipDisabled
+                  overlayEnabled
+                />
+              )}
+            </ParentSize>
+          </Paper>
         </Grid>
         <Grid item xs={12} md={6}>
           <Card variant="outlined">
-            <CardContent style={{display: "flex"}}>
+            <CardContent style={{ display: "flex" }}>
               <Box>
                 <Typography variant="subtitle2" component="div">
                   Total Borrows
                 </Typography>
                 <BorrowText variant="h4">
-                  {currencyFormatter.format(market.totalBorrows * market.underlyingPriceUSD)}
+                  {currencyFormatter.format(
+                    market.totalBorrows * market.underlyingPriceUSD
+                  )}
                 </BorrowText>
               </Box>
-              <Box ml={6} style={{marginLeft: "auto"}}>
+              <Box ml={6} style={{ marginLeft: "auto" }}>
                 <Typography variant="subtitle2" component="div">
                   Borrow APY
                 </Typography>
@@ -220,7 +244,7 @@ function LendingPage() {
           </Card>
           <Paper
             variant="outlined"
-            style={{ height: 300, marginTop: "40px", position: "relative"}}
+            style={{ height: 300, marginTop: "40px", position: "relative" }}
           >
             <ParentSize>
               {({ width, height }) => (
@@ -239,10 +263,19 @@ function LendingPage() {
         </Grid>
       </Grid>
 
-      <Grid container spacing={6} style={{ marginTop: "20px", height: "40%"}}>
+      <Grid container spacing={6} style={{ marginTop: "20px", height: "40%" }}>
         <Grid item xs={12} md={6}>
-          <Card variant="outlined" style={{ display: "flex", alignItems: "center" }}>
-            <CardContent style={{display: "flex", displayDirection: "col", marginRight: "100px"}}>
+          <Card
+            variant="outlined"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <CardContent
+              style={{
+                display: "flex",
+                displayDirection: "col",
+                marginRight: "100px",
+              }}
+            >
               <Box width="150px">
                 <Typography variant="subtitle2" component="div" nowrap>
                   Available Liquidity
@@ -252,22 +285,37 @@ function LendingPage() {
                 </SupplyText>
               </Box>
             </CardContent>
-            <ProgressBar style={{ width: '100%', marginRight: "20px"}} value={(liquidityUSD/totalLiquidityUSD) * 100}/>
+            <ProgressBar
+              style={{ width: "100%", marginRight: "20px" }}
+              value={(liquidityUSD / totalLiquidityUSD) * 100}
+            />
           </Card>
         </Grid>
         <Grid item xs={12} md={6}>
-          <Card variant="outlined" style={{ display: "flex", alignItems: "center" }}>
-            <CardContent style={{display: "flex", displayDirection: "col", marginRight: "20px"}}>
+          <Card
+            variant="outlined"
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <CardContent
+              style={{
+                display: "flex",
+                displayDirection: "col",
+                marginRight: "20px",
+              }}
+            >
               <Box width="150px">
                 <Typography variant="subtitle2" component="div">
                   Utilization Rate
                 </Typography>
                 <BorrowText variant="h4">
-                  { utilizationRate.toFixed(2) + "%" }
+                  {utilizationRate.toFixed(2) + "%"}
                 </BorrowText>
               </Box>
             </CardContent>
-            <ProgressBar style={{ width: '100%', marginRight: "20px"}} value={utilizationRate < 100 ? utilizationRate : 100}/>
+            <ProgressBar
+              style={{ width: "100%", marginRight: "20px" }}
+              value={utilizationRate < 100 ? utilizationRate : 100}
+            />
           </Card>
         </Grid>
       </Grid>
@@ -275,13 +323,15 @@ function LendingPage() {
         <Grid item xs={12} md={6}>
           <Paper
             variant="outlined"
-            style={{ height: 300, marginTop: "40px", position: "relative"}}
+            style={{ height: 300, marginTop: "40px", position: "relative" }}
           >
             <ParentSize>
               {({ width, height }) => (
                 <BarChart
                   title="Total Liquidations (USD)"
-                  data={liquidationChartDatas.underlyingCollateralSeizedAmountUSD}
+                  data={
+                    liquidationChartDatas.underlyingCollateralSeizedAmountUSD
+                  }
                   width={width}
                   height={height}
                   margin={{ top: 125, right: 0, bottom: 0, left: 0 }}
@@ -295,7 +345,7 @@ function LendingPage() {
         <Grid item xs={12} md={6}>
           <Paper
             variant="outlined"
-            style={{ height: 300, marginTop: "40px", position: "relative"}}
+            style={{ height: 300, marginTop: "40px", position: "relative" }}
           >
             <ParentSize>
               {({ width, height }) => (
@@ -314,7 +364,7 @@ function LendingPage() {
         </Grid>
       </Grid>
 
-      <Grid container spacing={4} style={{marginTop: "30px"}}>
+      <Grid container spacing={4} style={{ marginTop: "30px" }}>
         <Grid item xs={12} md={4}>
           <KPI
             title="Price"
@@ -324,7 +374,7 @@ function LendingPage() {
         <Grid item xs={12} md={4}>
           <KPI
             title="Reserve Factor"
-            value={Number((market.reserveFactor * 100)/1e18).toFixed(2) + "%"}
+            value={Number((market.reserveFactor * 100) / 1e18).toFixed(2) + "%"}
           />
         </Grid>
         <Grid item xs={12} md={4}>
@@ -339,11 +389,12 @@ function LendingPage() {
 }
 
 export async function getStaticProps({ params }) {
-  const APIURL = "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/lending";
+  const APIURL =
+    "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/lending";
 
   const client = new ApolloClient({
     uri: APIURL,
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
   });
 
   const id = params.id.toLowerCase();
@@ -351,24 +402,23 @@ export async function getStaticProps({ params }) {
   await client.query({
     query: marketQuery,
     variables: {
-      id: id
-    }
+      id: id,
+    },
   });
 
   await client.query({
     query: marketDayDatasQuery,
     variables: {
-      markets: [id]
-    }
+      markets: [id],
+    },
   });
 
   await client.query({
     query: liquidationDayDatasQuery,
     variables: {
-      seizedAddress: [id]
-    }
+      seizedAddress: [id],
+    },
   });
-
 
   return {
     props: {
@@ -379,20 +429,23 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const APIURL = "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/lending";
-  
+  const APIURL =
+    "https://api.thegraph.com/subgraphs/name/traderjoe-xyz/lending";
+
   const client = new ApolloClient({
     uri: APIURL,
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
   });
 
-  const { data: { markets } } = await client.query({
-    query: marketsQuery
-  })
+  const {
+    data: { markets },
+  } = await client.query({
+    query: marketsQuery,
+  });
 
-  const paths = markets.map(market => ({
+  const paths = markets.map((market) => ({
     params: { id: market.id },
-  }))
+  }));
 
   return { paths, fallback: true };
 }
